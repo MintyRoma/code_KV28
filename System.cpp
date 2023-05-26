7e57f691d1b1d044e74bc0e8e9185c9e66c2eb44
srdhe <srdhe4@yandex.ru>
2023-05-27 02:52:40 +0300

    Created basic components

--- System.cpp
+++ System.cpp
@@ -7,9 +7,19 @@ System::System(cl_base* parent, std::string name):cl_base(parent,name)
 
 void System::build_tree_objects()
 {
-    cl_base* root = this;
-    InputDevice Inp(this, "Input Device");
-    Controller
+    cl_base* inptdev = new InputDevice(this, "Input Device");
+    cl_base* screendev = new ScreenDevice(this, "Screen Device");
+    cl_base* ctrldev = new Controller(this, "Controller Device");
+    cl_base* cashdr = new CashDrawer(ctrldev, "Cash Drawer");
+    cl_base* changedr = new ChangeDrawer(ctrldev,"Change Drawer");
+    cl_base* tckprint = new TicketPrinter(ctrldev, "Ticket Printer");
+
+    
+}
+
+int System::exec_app()
+{
+    this->cl_base::print_tree();
 }
 
 
